9b0244699dcaf49d5961d4d933e654970e4d7a58
Groot-2001 <shivasilmawala841@gmail.com>
2023-07-25 21:37:53 +0530

    Authentication page designed

--- routes/index.js
+++ routes/index.js
@@ -51,4 +51,12 @@ router
     }
   });
 
+router.get("/register", (req, res, next) => {
+  res.render("register");
+});
+
+router.get("/login", (req, res, next) => {
+  res.render("login");
+});
+
 module.exports = router;
